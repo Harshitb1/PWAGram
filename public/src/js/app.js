531838033a4c7f719f4592cc720474f0e873b600
Harshitb1 <36952154+Harshitb1@users.noreply.github.com>
2019-01-29 12:58:23 +0530

    sw updated

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,6 @@
 
+var deferredPrompt;
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
@@ -6,3 +8,10 @@ if ('serviceWorker' in navigator) {
       console.log('Service worker registered!');
     });
 }
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
