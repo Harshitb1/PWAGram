789feac7a85af9f6dc8a8dfda20a797ca780dab3
Harshitb1 <36952154+Harshitb1@users.noreply.github.com>
2019-02-03 15:24:37 +0530

    notification click

--- functions/index.js
+++ functions/index.js
@@ -26,8 +26,30 @@ exports.storePostData = functions.https.onRequest((request, response) => {
             'BFTUzCozI_1pkPiMYp5FAE02EXsuzDqZdazVnS-JJwrczsiHTMPdPe_fYWX_x2Pho6ZozjFnZ-zJAKGR1z_XG-I',
             'RVyrCZ-Ha2ju6zHKSqwKFEMKJcvrOMZzV7v6PGKmY_A');
             return admin.database().ref('subscriptions').once('value');
-            response.status(201).json({message: 'Data Stored', id: request.body.id});
         })
+        .then(function (subscriptions) {
+            subscriptions.forEach(function (sub) {
+              var pushConfig = {
+                endpoint: sub.val().endpoint,
+                keys: {
+                  auth: sub.val().keys.auth,
+                  p256dh: sub.val().keys.p256dh
+                }
+              };
+    
+              webpush.sendNotification(pushConfig,
+                 JSON.stringify({
+                     title: 'New Post', 
+                     content: 'New Post added!',
+                     openUrl: '/help'
+                    })
+                )
+                .catch(function(err) {
+                  console.log(err);
+                })
+            });
+            response.status(201).json({message: 'Data stored', id: request.body.id});
+          })
         .catch(function(err){
             response.status(500).json({error: err});
         })
--- public/sw.js
+++ public/sw.js
@@ -234,10 +234,48 @@ self.addEventListener('notificationclick', function(event) {
     notification.close();
   } else {
     console.log(action);
-    notification.close();
+    event.waitUntil(
+      clients.matchAll()
+        .then(function(clis) {
+          var client = clis.find(function(c) {
+            return c.visibilityState === 'visible';
+          });
+
+          if (client !== undefined) {
+            client.navigate(notification.data.url);
+            client.focus();
+          } else {
+            clients.openWindow(notification.data.url);
+          }
+          notification.close();
+        })
+    );
   }
 });
 
 self.addEventListener('notificationclose', function(event) {
   console.log('Notification was closed', event);
+});
+
+self.addEventListener('push', function(event) {
+  console.log('Push Notification received', event);
+
+  var data = {title: 'New!', content: 'Something new happened!', openUrl: '/'};
+
+  if (event.data) {
+    data = JSON.parse(event.data.text());
+  }
+
+  var options = {
+    body: data.content,
+    icon: '/src/images/icons/app-icon-96x96.png',
+    badge: '/src/images/icons/app-icon-96x96.png',
+    data:{
+      url: data.openUrl
+    }
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
 });
