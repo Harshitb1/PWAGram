ec02c2330b4a0274af14670cac06503e801805b5
Harshitb1 <36952154+Harshitb1@users.noreply.github.com>
2019-02-03 14:02:32 +0530

    notification subscription

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 var functions = require('firebase-functions');
 var admin = require("firebase-admin");
 var cors = require('cors') ({ origin : true });
+var webpush = require('web-push');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -21,12 +22,15 @@ exports.storePostData = functions.https.onRequest((request, response) => {
             image: request.body.image
         })
         .then(function(){
+            webpush.setVapidDetails('mailto:harshitbatra56@yahoo.in',
+            'BFTUzCozI_1pkPiMYp5FAE02EXsuzDqZdazVnS-JJwrczsiHTMPdPe_fYWX_x2Pho6ZozjFnZ-zJAKGR1z_XG-I',
+            'RVyrCZ-Ha2ju6zHKSqwKFEMKJcvrOMZzV7v6PGKmY_A');
+            return admin.database().ref('subscriptions').once('value');
             response.status(201).json({message: 'Data Stored', id: request.body.id});
         })
         .catch(function(err){
-            
             response.status(500).json({error: err});
-        
         })
     });
 });
+  
--- public/src/js/app.js
+++ public/src/js/app.js
@@ -25,10 +25,73 @@ window.addEventListener('beforeinstallprompt', function(event) {
 });
 
 function displayConfirmNotification() {
-  var options = {
-    body: 'You successfully subscribed to our Notification service!'
-  };
-  new Notification('Successfully subscribed!', options);
+  if ('serviceWorker' in navigator) {
+    var options = {
+      body: 'You successfully subscribed to our Notification service!',
+      icon: '/src/images/icons/app-icon-96x96.png',
+      image: '/src/images/sf-boat.jpg',
+      dir: 'ltr',
+      lang: 'en-US', // BCP 47,
+      vibrate: [100, 50, 200],
+      badge: '/src/images/icons/app-icon-96x96.png',
+      tag: 'confirm-notification',
+      renotify: true,
+      actions: [
+        { action: 'confirm' , title: 'Okay', icon: '/src/images/icons/app-icon-96x96.png'},
+        { action: 'cancel' , title: 'Cancel', icon: '/src/images/icons/app-icon-96x96.png'}
+
+      ]
+    };
+
+    navigator.serviceWorker.ready
+      .then(function(swreg) {
+        swreg.showNotification('Successfully subscribed (from SW)!', options);
+      });
+  }
+}
+
+function configurePushSub() { 
+  if (!('serviceWorker' in navigator)) {
+    return;
+  }
+
+  var reg;
+  navigator.serviceWorker.ready
+    .then(function(swreg) {
+      reg = swreg;
+      return swreg.pushManager.getSubscription();
+    })
+    .then(function(sub) {
+      if (sub === null) {
+        // Create a new subscription 
+        var vapidPublicKey = 'BFTUzCozI_1pkPiMYp5FAE02EXsuzDqZdazVnS-JJwrczsiHTMPdPe_fYWX_x2Pho6ZozjFnZ-zJAKGR1z_XG-I';
+        var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+        return reg.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: convertedVapidPublicKey
+        });
+      } else {
+        // We have a subscription
+      }
+    })
+    .then(function(newSub) {
+      return fetch('https://pwagram-e16c1.firebaseio.com/subscriptions.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify(newSub)
+      })
+    })
+    .then(function(res) {
+      if (res.ok) {
+        displayConfirmNotification();
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
 function askForNotificationPermission() {
@@ -37,12 +100,13 @@ function askForNotificationPermission() {
     if (result !== 'granted') {
       console.log('No notification permission granted!');
     } else {
-      displayConfirmNotification();
+      // displayConfirmNotification();
+      configurePushSub();
     }
   });
 }
 
-if ('Notification' in window) {
+if ('Notification' in window && 'serviceWorker' in navigator) {
   for (var i = 0; i < enableNotificationsButtons.length; i++) {
     enableNotificationsButtons[i].style.display = 'inline-block';
     enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
